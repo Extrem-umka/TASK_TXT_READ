3808608af859d7dff7d3a5f466ce1e0b67050028
Extrem_umka <zhulianev@gmail.com>
2022-08-17 20:46:54 +0600

    add_TASK2

--- cook.py
+++ cook.py
@@ -17,3 +17,32 @@ def cook_book():
 
     return cook_book
 print(cook_book())
+
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+  }
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredients_list = dict()
+    for dish in dishes:
+        for ingredients in cook_book[dish]:
+            i = dict()
+            i['measure'] = ingredients['measure']
+            i['quantity'] = ingredients['quantity'] * person_count
+            ingredients_list[ingredients['ingredient_name']] = i
+    return ingredients_list
+print(get_shop_list_by_dishes(dishes=['Утка по-пекински', 'Омлет'], person_count=3))
