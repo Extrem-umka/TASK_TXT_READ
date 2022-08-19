30dd4b3d14a8af5e4e5158e481adec74eb2c0894
Extrem_umka <zhulianev@gmail.com>
2022-08-20 00:08:13 +0600

    change TASK3_iteration2

--- TASK3/TASK3.py
+++ TASK3/TASK3.py
@@ -1,9 +1,20 @@
-import os
-ls = [i for i in os.listdir() if i.endswith('.txt')]
-ls.sort()
-with open('result.txt','w', encoding='utf-8') as f:
-    for j in ls:
-        s = open((j),encoding='utf-8').read()
-        print(s)
-        f.write(s)
-        f.write('\n')
+texts = ['1.txt', '2.txt', '3.txt']
+
+dict_r = {}
+dict_w = {}
+
+for text in texts:
+    with open(text, encoding='utf-8') as f:
+        txt = f.read().split('\n')
+        s = len(txt)
+        dict_r[s] = text
+        dict_w[s] = txt
+
+sorted_dict_r = sorted(dict_r.items())
+print(sorted_dict_r)
+
+with open('result.txt', 'w', encoding='utf-8') as f:
+    for k, v in sorted_dict_r:
+        f.write(f'{str(k)}\n{v}\n')
+        for text in dict_w[k]:
+            f.write(f'{text}\n')
